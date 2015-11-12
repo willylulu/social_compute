429b8562fe57746bbb00d1f86ca7a1f8249fa396
LemonAtSu <at_7788546@hotmail.com>
2015-11-12 19:24:34 +0800

    modify socket in index.js

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.post('/', function(req, res) {
 });
 io.sockets.on('connection', function(socket) {
 	//console.log('type:' + socket.handshake.query.type);
+
 	socket.on('response_add_seller', function(data) {
 		// body...
 		console.log('response_add_seller:'+data);
@@ -64,6 +65,7 @@ io.sockets.on('connection', function(socket) {
 		new_channel.customers.push(data);
 		channels[data.id] = new_channel;
 	});
+
 	socket.on('response_buyer_id_seller_id', function(data) {
 		// body...
 		console.log('response_buyer_id_seller_id:'+data);
@@ -73,17 +75,52 @@ io.sockets.on('connection', function(socket) {
 		console.log('url:' + channel.youtube_url);
 		socket.emit('response_url', channels[data.seller_id].youtube_url);
 	});
-	socket.on('chat',function (fb_id,user_id,talk) {
-		console.log(fb_id);
-		var customers  = channels[fb_id].customers;
-		console.log(customers.length);
-		console.log(talk);
-		console.log(fb_id);
-		for (var i = 0; i < customers.length; i++) {
-			io.to(customers[i]).emit('chat',user_id,talk);
-			//console.log('talk');
-		};
+
+	socket.on('buyer_send_message', function (response) {
+        var seller_id = response.user.seller_id;
+		var customers  = channels[seller_id].customers;
+		
+        for (var key in dictionary) {
+			io.to(customers[key]).emit('msg_broadcast', response);
+		}
 	});
+
+    socket.on('buyer_change_price', function (response) {
+        var seller_id = response.user.seller_id;
+        var user_id   = response.user.id;
+        var price = response.price;
+		var customers  = channels[seller_id].customers;
+		
+        channels[seller_id].customers[user_id].price = price;
+ 
+        for (var key in customers) {
+            io.to(customers[key]).emit('buyer_price_broadcast', response);
+        } 
+    });
+
+    socket.on('seller_change_price', function (response) {
+        var seller_id = response.user.seller_id;
+		var customers  = channels[seller_id].customers;
+    
+        for (var key in customers) {
+            io.to(customers[key]).emit('seller_price_broadcast', response);
+        }
+    });
+
+    socket.on('select_product', function(response) {
+        var seller_id = response.user.seller_id;
+        var customers = channels[seller_id].customers;
+        var product_id = response.product.id;
+        var product_name = channels[seller_id].product_list[product_id];
+        response.product.name = product_name;
+        channels[seller_id].product.id = product_id;
+
+
+        for (var key in customers) {
+            io.to(customers[key]).emit('product_select_broadcast', response);
+        }
+    });
+
 	socket.on('disconnect',function () {
 		// body...
 		console.log(people[socket.id]+" disconnect!");
@@ -93,6 +130,6 @@ io.sockets.on('connection', function(socket) {
 			requests.post("localhost:8000/buyqueue", buyer_queue[socekt.id]);
 		
 		}
-	})
+	});
 });
 
