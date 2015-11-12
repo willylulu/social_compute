86a3cd1f2c60d428bcacf2565a403be560be6670
Frank <franks0059@gmail.com>
2015-11-12 19:36:22 +0800

    ><

--- public/js/chatroom_event.js
+++ public/js/chatroom_event.js
@@ -1,12 +1,18 @@
 $('#price_input').keypress(function(e){
-if(e.keyCode==13)
-    ChangePrice();
+if(e.keyCode==13){
+    var price_input = document.getElementById("msg-input");
+    var price = price_input.value;
+    price_input.innerHTML = '';
+    ChangePrice(price);
+    }
 });
 
 
-
-
 $('#msg_input').keypress(function(e){
-if(e.keyCode==13)
-    SendMessage(); 
+if(e.keyCode==13){
+    var msg_input = document.getElementById("msg-input");
+    var msg = msg_input.value;
+    msg_input.innerHTML = '';
+    SendMessage(msg);
+    } 
 });
