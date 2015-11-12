430b75a7511150d200d2e2e7f7a18e7fc5eaaca2
willylulu <maya6282@gmail.com>
2015-11-12 21:43:55 +0800

    add some update

--- public/js/saleman.js
+++ public/js/saleman.js
@@ -1,3 +1,5 @@
+var man_price_list = [];
+
 function SelectProduct(product_id)
 {
 	//clear 
@@ -28,5 +30,31 @@ function SellerConfirmProductOrder(customers)
 	sendObj.customers = customers;
 	socket.emit('seller_confirm_product_order',sendObj);
 }
+function UpdatePrice(response)
+{
+    //price list 
+    var customers = response.customers;
+    var wufa="";
+    for(var key in customers){
+        wufa+="<input type=\"checkbox\" name=\"bit\" class=\"bit\" value=\""+price+"\">"+"ID: "+man_price_list[i].id+" Price: "+man_price_list[i].price+"<br>"
+    }
+    $('#price-box').html(wufa);
+}
 
-function 
+function UpdateMessage(response) {
+
+    response.user.name;
+    var div = document.getElementById("msg-box");
+    var msg_div = document.createElement("div");
+
+    msg_div.className = "msg";
+    msg_div.innerHTML = response.user.name + " : " + response.msg + "<br>";
+    div.appendChild(msg_div);
+    div.scrollTop = div.scrollHeight; // scroll to bottom
+
+}
+function UpdateProduct(response)
+{
+    var wufa =response.product.name;
+    $('#productname').html(wufa);
+}
