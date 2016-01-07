c3f7fa6249ddda04e18911df94751fee55ca7a2b
Frank <franks0059@gmail.com>
2016-01-08 06:14:12 +0800

    frank edit at 5:00am QQ

--- hall/static/js/grayscale.js
+++ hall/static/js/grayscale.js
@@ -1,16 +1,22 @@
-/*!
+/*
  * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
 // jQuery to collapse the navbar on scroll
+
+var preScroll = 0;
+
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+
+    if (preScroll < $(this).scrollTop()) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
+    
+    preScroll = $(this).scrollTop();
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
