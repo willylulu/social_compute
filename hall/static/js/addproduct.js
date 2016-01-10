ee53c135034fcb3e3777648d2f05a4079e10b11e
willylulu <maya6282@gmail.com>
2016-01-10 22:25:48 +0800

    upload picture but not yet

--- hall/static/js/addproduct.js
+++ hall/static/js/addproduct.js
@@ -12,6 +12,13 @@ function submit () {
 	var name = $("#productnametext").val();
 	var price = $("#productpricetext").val();
 	var description = $('textarea').val();
+	var picture = $('#picture')[0];
+	var file = picture.files[0];
+	if (!file.type.match('image.*')) {
+		alarm("需為圖片格式!");
+	    return;
+	}
+	drawImage(file);
 	$.ajax({
 	      url:"http://tvsalestream.herokuapp.com/insertproduct/",
 	      type:"POST",
@@ -19,11 +26,20 @@ function submit () {
 	      dataType:"json"
 	}).done(function() {
 
-	}) ;
+	});
 	console.log('Done');
 	$('#productnametext').val('');
 	$('#productpricetext').val('');
 	$('textarea').val('');
+}
+function drawImage(file){
+	var img = new Image;
+	img.src = URL.createObjectURL(file);
+	img.onload = function() 
+	{
+		var c = $("#canvas")[0].getContext("2d");
+		c.drawImage(img,0,0);
+	}
 }
 //user
 //uid
