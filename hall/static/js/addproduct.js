78399f8a95d4a83217cbbe8580ffb5b68c984114
willylulu <maya6282@gmail.com>
2016-01-12 01:05:24 +0800

    upload image url

--- hall/static/js/addproduct.js
+++ hall/static/js/addproduct.js
@@ -14,32 +14,62 @@ function submit () {
 	var description = $('textarea').val();
 	var picture = $('#picture')[0];
 	var file = picture.files[0];
-	if (!file.type.match('image.*')) {
-		alarm("需為圖片格式!");
+	if (!file.type.match('image.png')) {
+		alarm("需為圖片(Png)格式!");
 	    return;
 	}
-	drawImage(file);
-	$.ajax({
-	      url:"http://tvsalestream.herokuapp.com/insertproduct/",
-	      type:"POST",
-	      data:{user:user,uid:uid,productname:name,price:price,description:description},
-	      dataType:"json"
-	}).done(function() {
-
+	var url = URL.createObjectURL(file);
+	upload(url,function(url) {
+		// body...
+		console.log(url);
+		$.ajax({
+		      url:"http://tvsalestream.herokuapp.com/insertproduct/",
+		      type:"POST",
+		      data:{user:user,uid:uid,productname:name,price:price,description:description,image_url:url},
+		      dataType:"json"
+		}).done(function() {});
 	});
 	console.log('Done');
 	$('#productnametext').val('');
 	$('#productpricetext').val('');
 	$('textarea').val('');
 }
-function drawImage(file){
-	var img = new Image;
-	img.src = URL.createObjectURL(file);
-	img.onload = function() 
-	{
-		var c = $("#canvas")[0].getContext("2d");
-		c.drawImage(img,0,0);
-	}
-}
 //user
 //uid
+function convertToDataURLviaCanvas(url, callback, outputFormat){
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        var dataURL;
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+        canvas = null; 
+    };
+    img.src = url;
+}
+function upload (url,callback) {
+	// body...
+	convertToDataURLviaCanvas(url,function(base64Img){
+		var temp = base64Img.replace("data:image/png;base64,", "");
+		console.log(temp);
+		$.ajax({ 
+		    url: 'https://api.imgur.com/3/upload',
+		    headers: {
+		        'Authorization': 'Client-ID d683f0b0ac51745'
+		    },
+		    type: 'POST',
+		    data: {
+		    	'type': 'base64',
+		        'image': temp
+		    },
+		    success: function(res) {
+		    	callback(res.data.link);
+		    }
+		});
+	},'image/png');
+}
