de4eb0f5069597f1732652a10561044cc4c524bd
willylulu <maya6282@gmail.com>
2016-01-13 19:32:25 +0800

    put image

--- hall/static/js/productlist.js
+++ hall/static/js/productlist.js
@@ -21,13 +21,19 @@ function setModalAttribute(pk){
     var description = productlist[pk]['description'];
     var title = productlist[pk]['title'];
     var price = productlist[pk]['price'];
+    var imgurl = productlist[pk]['imgurl'];
     $('.modaldescription').append(description);
     $('.modaltitle').text(title);
+    var img = document.createElement("img");
+    img.setAttribute('src',imgurl);
+    img.setAttribute('width',570);
+    $('.modalimage').append(img);
 }
 
 function cleanModal(){
     $('.modaldescription').empty();
     $('.modaltitle').empty();
+    $('.modalimage').empty();
 }
 
 $('.productblock').click(function(){
