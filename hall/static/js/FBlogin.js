5cd3431e59e74d165124149194b74938a815ebdd
LemonAtSu <at_7788546@hotmail.com>
2016-01-27 22:09:43 +0800

    Now only contain backend communicate function. no more stupid sdk

--- hall/static/js/FBlogin.js
+++ hall/static/js/FBlogin.js
@@ -1,45 +1,24 @@
 var check_url = "http://tvsalestream.herokuapp.com/checklogin/";
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '938533709533376',
-      xfbml      : true,
-      version    : 'v2.5'
-    });
-    checkLoginState(function () {
-      FBinitCallback();
-    });
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-function checkLoginState(callback) {
-      FB.getLoginStatus(function(response) {
-        console.log(response);
-
-        if(response && response.status === 'connected') {
-            me=response;
-        }
-        if(callback && response) {
-          callback(response.status);
-        }
-      });
+function loginFacebook() {
+  $.get('../auth/islogin', function(response) {
+    if(!response) {
+      // goto login page.
+      console.log('try to login');
+      window.location = "../auth/facebook";
+    } else {
+      // already logged.
+      console.log(response);
     }
+  });
+}
 
-    function loginFacebook() {
-      checkLoginState(function(resp){
-          if(resp != 'connected') {
-            console.log('not login.');
-            FB.login(function (response) {
-              if(response.authResponse) {
-                location.reload();
-              }
-            });
-          }
-      });
+function checkLogin() {
+  $.get('../auth/islogin', function(response) {
+    if(response) {
+      // do some change.
     }
+  });
+
+}
+
