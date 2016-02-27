6a9921f90135257182965eb62a3b2f593119f559
LemonAtSu <at_7788546@hotmail.com>
2016-02-27 22:40:55 +0800

    Reorganize code to make it more readable and more easy to maintain

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ var express    = require('express'),
     channels   = new Object();
 
 //  share channel data to all backend port
+module.exports.app      = app;
+module.exports.engine   = engines;
+module.exports.request  = request;
+module.exports.express  = express;
 module.exports.channels = channels;
-module.exports.app = app;
 
 // start web mapping
 require('./web.js');
@@ -113,23 +116,24 @@ app.post('/create_channel', function(req, res) {
     req.on('end', function() {
         //body = qs.parse(body);
         //console.log(body);
-        var data = JSON.parse(body);
-        var host_fb_id = data.hostfbid; // use fb id as identifier  
+        var data        = JSON.parse(body);
+        var host_fb_id  = data.hostfbid; // use fb id as identifier  
         var ProductList = data.productlist;
-        var streamurl = data.streamurl; 
-        var host_name = data.hostname;      
-
+        var streamurl   = data.streamurl; 
+        var host_name   = data.hostname;      
+        var channel     = channels[host_fb_id];
         // put basic info into target channel.
         // other info such as stream url and socket id
         // will be init when create_channel socket catch data.
-        channels[host_fb_id] = new Object();
-        channels[host_fb_id].ProductList = ProductList;
-        channels[host_fb_id].CurrentProduct = 0;
-        channels[host_fb_id].PriceList = new Object();
-        channels[host_fb_id].customers = new Object();
-        channels[host_fb_id].stream_url = streamurl;
-        channels[host_fb_id].host_name = host_name;   
-        channels[host_fb_id].customer_length = 0; 
+        channel                 = new Object();
+        channel.PriceList       = new Object();
+        channel.customers       = new Object();
+        channel.customer_length = 0; 
+        channel.CurrentProduct  = 0;
+        channel.stream_url      = streamurl;
+        channel.host_name       = host_name;   
+        channel.ProductList     = ProductList;
+
         res.render(room_route + 'chatroom.html', data);
 
     });
@@ -147,146 +151,164 @@ io.sockets.on('connection', function(socket) {
         // host_fb_id
         // stream_url
         console.log(req);
-        var sendObj = new Object();
+
         var host_fb_id = req.host_fb_id;
+        var channel    = channels[host_fb_id];
+        var host       = new Object();
+        var sendObj    = new Object();
 
         if(!host_fb_id)
             return;
 
-        if(channels[host_fb_id].socket_id) {
-            channels[host_fb_id].socket_id = socket.id;
-            sendObj.ProductList = channels[host_fb_id].ProductList;
-            sendObj.CurrentProduct = channels[host_fb_id].CurrentProduct;
-            io.to(channels[host_fb_id].socket_id).emit('update_productlist', sendObj); // undefine.
-            return;
+        if(!(channel.socket_id)) {
+            host.user                    = req;
+            host.socket_id               = socket.id;
+            channel.socket_id            = socket.id;
+            channel.customers[socket.id] = host;
         }
-        var host = new Object();
-        host.user = req;
-        host.socket_id = socket.id;
 
-        channels[host_fb_id].socket_id = socket.id;
-        var customer = new Object();
-        customer.user = req;
-        customer.socket_id = socket.id;
-        channels[host_fb_id].customers[host_fb_id] = customer;
         // Put host into customer list to recieve chat msg.
-        //channels[host_fb_id].customers[host_fb_id] = host;
-        sendObj.ProductList = channels[host_fb_id].ProductList;
-        sendObj.CurrentProduct = 0;
+        sendObj.ProductList    = channel.ProductList;
+        sendObj.CurrentProduct = channel.CurrentProduct;
         io.to(socket.id).emit('update_productlist', sendObj); // undefine.
     });
 
     socket.on('enter_channel', function(req) {
         // response will be a 'me' object.
-        //console.log(req);
-        var user_id = req.fb_id;
-        var customer = new Object();
+        var user_id    = req.fb_id;
         var host_fb_id = req.host_fb_id;
-        var sendObj = new Object();
+        var channel    = channels[host_fb_id];
+        var customer   = new Object();
+        var sendObj    = new Object();
+        var announce   = new Object();
 
-        if(!host_fb_id)
+        if(!channel)
             return;
 
-        customer.user = req;
+        customer.user      = req;
         customer.socket_id = socket.id;
-        // put customer into channel's customers list.
 
-        //channels[host_fb_id].customers[user_id] = customer;
-        channels[host_fb_id].customers[socket.id] = customer;
-        channels[host_fb_id].customer_length ++;
-        sendObj.CurrentProduct = channels[host_fb_id].CurrentProduct;
-        sendObj.ProductList = channels[host_fb_id].ProductList;
+        // put customer into channel's customers list.
+        sendObj.ProductList          = channel.ProductList;
+        sendObj.CurrentProduct       = channel.CurrentProduct;
+        channel.customers[socket.id] = customer;
+        channel.customer_length ++;
+
+
+        announce.new_customer    = req;
+        announce.customer_length = channel.customer_length;
 
         io.to(socket.id).emit('update_productlist', sendObj);
-
-        var announce = new Object();
-        announce.new_customer = req;
-        announce.customer_length = channels[host_fb_id].customer_length;
-        io.to(channels[host_fb_id].socket_id).emit('customer_enter', announce);
+        io.to(channel.socket_id).emit('customer_enter', announce);
     });
 
     socket.on('send_msg', function(req) {
         console.log(req);
         var host_fb_id = req.user.host_fb_id;
-        var customers = channels[host_fb_id].customers;
+        var channel    = channels[host_fb_id];
+        var customers;
+
+        if(!channel)
+            return;
+
+        customers = channel.customers;
 
         // If its host, add host name to it
-        if(socket.id === channels[host_fb_id].socket_id)
-            req.user.ufb_name = channels[host_fb_id].host_name;
+        if(socket.id === channel.socket_id)
+            req.user.ufb_name = channel.host_name;
 
         for (var key in customers) {
             io.to(key).emit('broadcast_msg', req);
         }
-        io.to(channels[host_fb_id].socket_id).emit('broadcast_msg', req);
     });
 
     socket.on('customer_change_price', function(req) {
-        var price_info = new Object();
         var host_fb_id = req.user.host_fb_id;
-        var customers = channels[host_fb_id].customers;
-        var sort_user = [];
+        var channel    = channels[host_fb_id];
+        var sort_user  = [];
+        var price_info = new Object();
+        var customers;
+
+        if(!channel)
+            return;
+
         // get price and user info from req.
-        price_info = req;
+        price_info       = req;
         price_info.price = parseInt(price_info.price);
-        //if(Number.isInteger(price_info.price)){
-            channels[host_fb_id].PriceList[req.user.fb_id] = price_info;
-            sort_user = Object.keys(channels[host_fb_id].PriceList);
-            sort_user.sort(function(a,b){return channels[host_fb_id].PriceList[b].price-channels[host_fb_id].PriceList[a].price;});
-            for (var key in customers) {
-                io.to(key).emit('broadcast_customer_price',channels[host_fb_id].PriceList,sort_user);
-            }
-            io.to(channels[host_fb_id].socket_id).emit('broadcast_customer_price', channels[host_fb_id].PriceList,sort_user);
-       // }
+        customers        = channel.customers;
+        channel.PriceList[req.user.fb_id] = price_info;
+
+        // sort user by price.
+        sort_user = Object.keys(channel.PriceList);
+        sort_user.sort(function (a, b) {
+            return channel.PriceList[b].price - channel.PriceList[a].price;
+        });
+        
+        for (var key in customers) {
+            io.to(key).emit('broadcast_customer_price', channel.PriceList, sort_user);
+        }
     });
 
     socket.on('host_change_price', function(req) {
         var host_fb_id = req.user.host_fb_id;
         var new_price  = req.new_price;
-        var customers  = channels[host_fb_id].customers;
-        var sendObj = new Object();
+        var channel    = channels[host_fb_id];
+        var sendObj    = new Object();
+        var customer;
+        var cur;
 
-        if(channels[host_fb_id]) {
-            var cur = channels[host_fb_id].CurrentProduct;
-            channels[host_fb_id].ProductList[cur].price = new_price;
-            sendObj.ProductList = channels[host_fb_id].ProductList;
-            sendObj.CurrentProduct = channels[host_fb_id].CurrentProduct;
+        if(!channel)
+            return;
 
-            for(var key in customers) {
-                io.to(key).emit('broadcast_host_product', sendObj);
-            }
-            io.to(channels[host_fb_id].socket_id).emit('broadcast_host_product', sendObj);
+        cur       = channel.CurrentProduct;
+        customers = channel.customers;
+            
+        channel.ProductList[cur].price = new_price;
+        sendObj.ProductList            = channel.ProductList;
+        sendObj.CurrentProduct         = channel.CurrentProduct;
+
+        for(var key in customers) {
+            io.to(key).emit('broadcast_host_product', sendObj);
         }
     });
 
     socket.on('select_product', function(req) {
         // need to be implemented
-        var host_fb_id = req.user.host_fb_id;
+        var host_fb_id  = req.user.host_fb_id;
+        var channel     = channels[host_fb_id];
+        var sendObj     = new Object();
+        var changeOrder = new Object();
         var customers;
         var orderlist;
-        var sendObj = new Object();
-        console.log('Select : ' + host_fb_id);
-        if(channels[host_fb_id]) {
-            channels[host_fb_id].CurrentProduct = req.new_pos;
-            if(!channels[host_fb_id].ProductList[req.new_pos].order) {
-                channels[host_fb_id].ProductList[req.new_pos].order = new Object();
-                channels[host_fb_id].ProductList[req.new_pos].order_length = 0;
-            }
+        var cur;
 
-            customers = channels[host_fb_id].customers;
-            orderlist = channels[host_fb_id].ProductList[req.new_pos].order;
-            sendObj.CurrentProduct = req.new_pos;
-            sendObj.host_fb_id = host_fb_id;
-            for(var key in customers) {
-                sendObj.order = orderlist[customers[key].user.fb_id];
-                io.to(key).emit('broadcast_product_select', sendObj);
-            }
-            var changeOrder = new Object();
-            changeOrder.newOrder = false;
-            changeOrder.order_length = channels[host_fb_id].ProductList[req.new_pos].order_length;
-            // only need to tell host that order status has change.
-            io.to(channels[host_fb_id].socket_id).emit('change_host_orderstatus', changeOrder);
+        console.log('Select : ' + host_fb_id);
+
+        if(!channel)
+            return;
+
+        channel.CurrentProduct = req.new_pos;
+        cur                    = channel.CurrentProduct;
+
+        if(!channel.ProductList[cur].order) {
+            channel.ProductList[cur].order = new Object();
+            channel.ProductList[cur].order_length = 0;
         }
 
+        customers              = channel.customers;
+        orderlist              = channel.ProductList[cur].order;
+        sendObj.CurrentProduct = cur;
+        sendObj.host_fb_id     = host_fb_id;
+
+        for(var key in customers) {
+            sendObj.order = orderlist[customers[key].user.fb_id];
+            io.to(key).emit('broadcast_product_select', sendObj);
+        }
+        
+        changeOrder.newOrder     = false;
+        changeOrder.order_length = channel.ProductList[cur].order_length;
+        // only need to tell host that order status has change.
+        io.to(channel.socket_id).emit('change_host_orderstatus', changeOrder);
     });
 
     socket.on('host_confirm_order', function(req) {
@@ -295,44 +317,56 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('customer_disconnect', function(req) {
         var host_fb_id = req.user.host_fb_id;
-        if(channels[host_fb_id].customers[socket.id]) {
-            console.log('customer leave : ' + channels[host_fb_id].customers[socket.id].user.fb_id + ' '+ socket.id);
-            delete channels[host_fb_id].customers[socket.id];
-            channels[host_fb_id].customer_length --;
+        var channel    = channels[host_fb_id];
+        if(channel.customers[socket.id]) {
+            console.log('customer leave : ' + channel.customers[socket.id].user.fb_id + ' '+ socket.id);
+            delete channel.customers[socket.id];
+            channel.customer_length --;
         }
     });
 
     socket.on('host_disconnect', function(req) {
         var host_fb_id = req.user.host_fb_id;
-        if(channels[host_fb_id]) {
-            var data = {
-                        owner     : host_fb_id,
-                        orderlist : JSON.stringify(channels[host_fb_id].ProductList),
-                        };
-            //data = JSON.stringify(data);
-            request.post({url : db_url + 'placeorder/', form : data}, function(response) {
+        var channel    = channels[host_fb_id];
+
+        if(!channel)
+            return;
+
+        var data = {
+            owner     : host_fb_id,
+            orderlist : JSON.stringify(channels[host_fb_id].ProductList),
+        };
+        
+        request.post({ 
+            url : db_url + 'placeorder/', 
+            form : data
+        }, function (response) {
                 // to do  :: retry if failed
                 console.log(response);
                 console.log('host finish : ' + host_fb_id);
-            });
-            delete channels[host_fb_id];
-        }
+        });
+        delete channel;
     });
 
     socket.on('change_customer_orderstatus', function(req) {
-        var uid = req.user.fb_id;
+        var uid        = req.user.fb_id;
+        var order      = req.order;
         var host_fb_id = req.user.host_fb_id;
-        if(!channels[host_fb_id]) {
-            return;
-        }
+        var channel    = channels[host_fb_id];
+        var sendObj    = new Object();
+        var cur;
+        var cur_product;
 
-        var order = req.order;
-        var cur = channels[host_fb_id].CurrentProduct;
-        var cur_product = channels[host_fb_id].ProductList[cur];
+        if(!channel) 
+            return;
+
+        cur         = channel.CurrentProduct;
+        cur_product = channel.ProductList[cur];
 
         if(!order && cur_product.order[uid]) {
             // custmoer want to cancel order.
             delete cur_product.order[uid];
+
             if(cur_product.order_length > 0)
                 cur_product.order_length--;
 
@@ -352,11 +386,10 @@ io.sockets.on('connection', function(socket) {
             return;
         }
 
-        var sendObj = new Object();
-        sendObj.newOrder = order;
+        sendObj.newOrder     = order;
         sendObj.order_length = cur_product.order_length;
         // only need to tell host that order status has change.
-        io.to(channels[host_fb_id].socket_id).emit('change_host_orderstatus', sendObj);
+        io.to(channel.socket_id).emit('change_host_orderstatus', sendObj);
     });
 
 });
