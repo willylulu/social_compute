76b78cb58c14cbb1f6bbe4838e2dbb0187c116a1
LemonAtSu <at_7788546@hotmail.com>
2016-03-01 01:25:15 +0800

    add insert methods

--- database/controller/Product.js
+++ database/controller/Product.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose'),
       wrap     = require('co-express'),
-      Product  = mongoose.model('Product');
+      Product  = mongoose.model('Product'),
+      User     = mongoose.model('User');
 
 
 exports.insert = wrap (function * (req, res) {
-    console.log(req);
+    try {
+        const uid  = req.body.uid;
+        const user = yield User.load(uid);
+        const product = new Product({
+            name        : req.body.productname,
+            price       : req.body.price,
+            description : req.body.description,
+            imgurl      : req.body.image_url,
+            uid         : uid
+        });
 
-    console.log(req.body);
+        yield product.save();
+        yield user.addProduct(product._id);
+
+        console.log('insert product to ' + uid);
+
+        res.sendStatus(400);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(400);
+    } 
 });
