27db2f608da72f54d3d6ca653b64d749d7b561c6
LemonAtSu <at_7788546@hotmail.com>
2016-03-01 01:22:51 +0800

    add user add/remove product methods

--- database/models/User.js
+++ database/models/User.js
@@ -3,11 +3,32 @@ const mongoose = require('mongoose'),
       ObjectId = Schema.ObjectId;
 
 const UserSchema = new Schema({
-    uid       : {type : String, default : '', trim : true, required : true},
-    name      : {type : String, default : '', trim : true, required : true},
-    streamurl : {type : String, default : '', trim : true},
+    uid         : {type : String, default : '', trim : true, required : true},
+    name        : {type : String, default : '', trim : true, required : true},
+    streamurl   : {type : String, default : '', trim : true},
+    products    : [ObjectId],
     online    : Boolean
 }, {_id : false});
+
+UserSchema.methods = {
+
+    addProduct    : function (pid) {
+        this.products.push(pid);
+        return this.save();
+    },
+
+    removeProduct : function (pid) {
+        const index = this.products.indexOf(pid);
+
+        if(~index) {
+            this.products.splice(index);
+        } else {
+            throw new Error('product not exist');
+        }
+
+        return this.save();
+    }
+}
 
 UserSchema.statics = {
 
