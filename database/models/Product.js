925f3d329230e468517ae26090ad77c78e668495
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-01 17:26:26 +0800

    REACTNAV

--- database/models/Product.js
+++ database/models/Product.js
@@ -18,7 +18,7 @@ ProductSchema.methods = {
 ProductSchema.statics = {
 
     load : function (_id) {
-        return this.findOne({ _id })
+        return this.findOne( {_id} )
                .populate('owner', 'name')
                .exec();
     },
