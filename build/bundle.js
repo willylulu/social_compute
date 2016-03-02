9162f97d3f9248be74fb087749839118fb8b6f2b
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-02 14:07:27 +0800

    bug in mongoose package

--- build/bundle.js
+++ build/bundle.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	(function webpackMissingModule() { throw new Error("Cannot find module \"./component/navbar.jsx\""); }());
+	(function webpackMissingModule() { throw new Error("Cannot find module \"./component/navbar.js\""); }());
 
 
 /***/ }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var path = require('path');
-
 module.exports = {
     entry: [
-        './component/navbar.jsx'
+        './component/navbar.js'
     ],
     output: {
         path: path.join(__dirname, '/build/'),
