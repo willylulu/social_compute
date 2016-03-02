928e3ae24f811fc7866cf8285af417c84e9ca49d
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-02 13:57:40 +0800

    fixed untracked files

--- component/navbar.jsx
+++ component/navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 const styles = {
+    button:{
 
+    },
+    nav:{
 
+    },
 };
 
 
 const NavBar = React.createClass({
-
-
+    
     render(){
         return (
-                <div className="navbar"></div>
+                <nav className="navbar"></nav>
                 );
     }
 });
